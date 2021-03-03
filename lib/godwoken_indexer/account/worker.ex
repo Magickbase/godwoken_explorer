ad573b60b6009c55660848758819f9d2bf252c15
Miles Zhang <mingchang555@hotmail.com>
2021-03-03 23:43:00 +0800

    feat: cal layer2 eth address
    Signed-off-by: Miles Zhang <mingchang555@hotmail.com>

--- lib/godwoken_indexer/account/worker.ex
+++ lib/godwoken_indexer/account/worker.ex
@@ -26,20 +26,21 @@ defmodule GodwokenIndexer.Account.Worker do
       script_hash = fetch_script_hash(account_id)
       script = fetch_script(script_hash)
       type = switch_account_type(script["code_hash"], script["args"])
+      eth_address = account_id_to_eth_adress(account_id, false)
 
       Account.create_or_update_account(%{
         id: account_id,
         script: script,
         script_hash: script_hash,
         type: type,
-        nonce: nonce
+        nonce: nonce,
+        eth_address: "0x" <> eth_address
       })
     end)
 
     {:noreply, [state]}
   end
 
-  # FIXME: Add other type condition by Script code_hash
   defp switch_account_type(code_hash, args) do
     polyjuice_code_hash = Application.get_env(:godwoken_explorer, :polyjuice_validator_code_hash)
     layer2_lock_code_hash = Application.get_env(:godwoken_explorer, :layer2_lock_code_hash)
@@ -56,7 +57,7 @@ defmodule GodwokenIndexer.Account.Worker do
     end
   end
 
-  def fetch_script_hash(account_id) do
+  defp fetch_script_hash(account_id) do
     options = Application.get_env(:godwoken_explorer, :json_rpc_named_arguments)
 
     case FetchedScriptHash.request(%{account_id: account_id})
@@ -66,7 +67,7 @@ defmodule GodwokenIndexer.Account.Worker do
     end
   end
 
-  def fetch_script(script_hash) do
+  defp fetch_script(script_hash) do
     options = Application.get_env(:godwoken_explorer, :json_rpc_named_arguments)
 
     case FetchedScript.request(%{script_hash: script_hash})
@@ -76,13 +77,24 @@ defmodule GodwokenIndexer.Account.Worker do
     end
   end
 
-  def fetch_nonce(account_id) do
+  defp fetch_nonce(account_id) do
     options = Application.get_env(:godwoken_explorer, :json_rpc_named_arguments)
 
     case FetchedNonce.request(%{account_id: account_id})
          |> HTTP.json_rpc(options) do
       {:ok, nonce} -> nonce
       {:error, _error} -> nil
+    end
+  end
+
+  # ethabi default is false.When use in evm set to true
+  defp account_id_to_eth_adress(account_id, ethabi) do
+    tail_binary_part = List.duplicate(0, 16) |> :binary.list_to_bin
+    head_binary_part = List.duplicate(0, 12) |> :binary.list_to_bin
+    if ethabi do
+      (head_binary_part <> <<account_id::32-little>> <> tail_binary_part) |> Base.encode16(case: :lower)
+    else
+      <<account_id::32-little>> <> tail_binary_part |> Base.encode16(case: :lower)
     end
   end
 end
