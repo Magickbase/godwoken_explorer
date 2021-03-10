1bd0e26bcaf6f80e91e4c8370b088a1d254a81fe
Chen Yu <keithwhisper@gmail.com>
2021-03-10 17:14:14 +0800

    feat: format balance with sudt decimal (#1)

--- lib/godwoken_explorer_web/controllers/api/account_controller.ex
+++ lib/godwoken_explorer_web/controllers/api/account_controller.ex
@@ -3,17 +3,49 @@ defmodule GodwokenExplorerWeb.API.AccountController do
 
   alias GodwokenExplorer.{Repo, Account}
 
-  def show(conn, params) do
-    result = case Repo.get(Account, params["id"]) do
-      %Account{} -> Account.find_by_id(params["id"])
-      nil ->  %{
-        error_code: 404,
-        message: "not found"
-      }
-    end
+  def show(conn, %{"id" => id}) do
+    result =
+      case Repo.get(Account, id) do
+        %Account{} ->
+          Account.find_by_id(id)
+          |> account_to_view()
+
+        nil ->
+          %{
+            error_code: 404,
+            message: "not found"
+          }
+      end
+
     json(
       conn,
       result
     )
+  end
+
+  defp account_to_view(account) do
+    account = %{account | ckb: balance_to_view(account.ckb, 8)}
+
+    with udt_list when not is_nil(udt_list) <- Kernel.get_in(account, [:user, :udt_list]) do
+      Kernel.put_in(
+        account,
+        [:user, :udt_list],
+        udt_list
+        |> Enum.map(fn udt ->
+          %{udt | balance: balance_to_view(udt.balance, udt.decimal)}
+        end)
+      )
+    else
+      _ ->
+        account
+    end
+  end
+
+  defp balance_to_view(balance, decimal) do
+    {val, _} = Integer.parse(balance)
+    (val / :math.pow(10, decimal)) |> :erlang.float_to_binary(decimals: decimal)
+  rescue
+    _ ->
+      balance
   end
 end
