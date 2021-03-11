92c2e9d4b9af3381e627e255d95e603b1e5226a6
Miles Zhang <mingchang555@hotmail.com>
2021-03-11 16:42:45 +0800

    fix: after join home channel return tx latest 10 records and statistic
    Signed-off-by: Miles Zhang <mingchang555@hotmail.com>

--- lib/godwoken_explorer_web/channels/home_channel.ex
+++ lib/godwoken_explorer_web/channels/home_channel.ex
@@ -4,14 +4,25 @@ defmodule GodwokenExplorerWeb.HomeChannel do
   """
   use GodwokenExplorerWeb, :channel
 
-  alias GodwokenExplorer.Block
+  alias GodwokenExplorer.{Block, Transaction, Chain}
 
   intercept(["home"])
 
   def join("home:refresh", _params, socket) do
     blocks = Block.latest_10_records()
+    txs = Transaction.latest_10_records()
+    account_count = Chain.account_estimated_count()
+    tx_count = Chain.transaction_estimated_count()
+    tps = Block.transactions_count_per_second()
 
-    {:ok, %{block_list: blocks}, socket}
+    statistic = %{
+      block_count: (blocks |> List.first() |> Map.get(:number) |> String.to_integer()) + 1,
+      tx_count: tx_count|> Integer.to_string(),
+      tps: tps |> Float.to_string(),
+      account_count: account_count |> Integer.to_string()
+    }
+
+    {:ok, %{block_list: blocks, tx_list: txs, statistic: statistic}, socket}
   end
 
   @spec handle_out(
