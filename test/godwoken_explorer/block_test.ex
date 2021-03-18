f951b116a1db806b4a850c31ea7af966ad49b39e
Miles Zhang <mingchang555@hotmail.com>
2021-03-18 18:29:31 +0800

    test: add block model test
    Signed-off-by: Miles Zhang <mingchang555@hotmail.com>

--- test/godwoken_explorer/block_test.ex
+++ test/godwoken_explorer/block_test.ex
@@ -2,7 +2,15 @@ defmodule GodwokenExplorer.BlockTest do
   use GodwokenExplorer.DataCase
 
   alias Ecto.Changeset
-  alias GodwokenExplorer.Block
+  alias GodwokenExplorer.{
+    Block,
+    Transaction
+  }
+  alias GodwokenExplorer.Chain.Cache.Blocks
+
+  setup do
+    ExMachina.Sequence.reset()
+  end
 
   describe "changeset/2" do
     test "with valid attributes" do
@@ -18,62 +26,107 @@ defmodule GodwokenExplorer.BlockTest do
     end
   end
 
-  describe "create/1" do
+  describe "create_block/1" do
     test "create success" do
+      params_list = :block |> params_for()
+      assert {:ok, %Block{}} = Block.create_block(params_list)
     end
 
     test "create failed" do
+      params_list = :block |> params_for(%{timestamp: nil})
+      assert {:error, %Ecto.Changeset{valid?: false}} = Block.create_block(params_list)
     end
   end
 
   describe "find_by_number_or_hash/1" do
+    setup do
+      insert(:block)
+
+      :ok
+    end
+
     test "find by number" do
+      assert %Block{number: 100} =
+        Block.find_by_number_or_hash(100)
     end
 
     test "find by hash" do
+      assert %Block{hash: "0x10047c3a847ede67fdad6357e7bf0a017fdb1be3e437a675a742e268af39bc3d"} =
+        Block.find_by_number_or_hash("0x10047c3a847ede67fdad6357e7bf0a017fdb1be3e437a675a742e268af39bc3d")
     end
 
     test "find no record" do
+      refute(Block.find_by_number_or_hash(10))
     end
   end
 
   describe "get_next_number/0" do
     test "with alreday exist record" do
+      insert(:block)
+      assert 101 = Block.get_next_number()
     end
 
     test "with empty record" do
+      assert 0 = Block.get_next_number()
     end
   end
 
   describe "latest_10_records/0" do
-     test "with cache" do
+     test "when no cache" do
+      insert_list(11, :block)
+      assert %{number: 110} = Block.latest_10_records() |> List.first()
+      assert %{number: 101} = Block.latest_10_records() |> List.last()
+      assert 10 = Block.latest_10_records() |> Enum.count()
      end
 
-     test "when no cache" do
+     test "with cache" do
+      build_list(11, :block) |> Enum.each(fn block ->
+        Blocks.update(block)
+      end)
+      assert %{number: 110} = Block.latest_10_records() |> List.first()
+      assert %{number: 101} = Block.latest_10_records() |> List.last()
+      assert 10 = Block.latest_10_records() |> Enum.count()
      end
   end
 
   describe "transactions_count_per_second" do
     test "with exist records" do
+      insert_list(10, :block)
+      assert 1.1 = Block.transactions_count_per_second()
     end
 
     test "with empty records" do
+      assert 0.0 = Block.transactions_count_per_second()
     end
   end
 
   describe "update_blocks_finalized" do
     test "with updated blocks" do
+      insert_list(10, :block)
+      Block.update_blocks_finalized(105)
+      assert 6 = from(b in Block, where: b.status == :finalized) |> Repo.aggregate(:count)
+      assert 6 = from(t in Transaction, where: t.status == :finalized) |> Repo.aggregate(:count)
     end
 
     test "no blocks updated" do
+      insert_list(10, :block)
+      Block.update_blocks_finalized(99)
+      assert 0 = from(b in Block, where: b.status == :finalized) |> Repo.aggregate(:count)
+      assert 0 = from(t in Transaction, where: t.status == :finalized) |> Repo.aggregate(:count)
     end
   end
 
   describe "bind_l1_l2_block/3" do
     test "bind succeed" do
+      insert(:block, %{layer1_block_number: nil, layer1_tx_hash: nil})
+      assert 3100 = Block.bind_l1_l2_block(100, 3100, "0x01")
+      assert %Block{layer1_block_number: 3100, layer1_tx_hash: "0x01"} = Repo.get_by(Block, number: 100)
     end
 
     test "no block need bind" do
+      insert(:block, %{layer1_block_number: nil, layer1_tx_hash: nil})
+      refute(Block.bind_l1_l2_block(101, 3100, "0x01"))
+      assert %Block{layer1_block_number: nil, layer1_tx_hash: nil} = Repo.get_by(Block, number: 100)
     end
   end
 end
--- test/support/factory.ex
+++ test/support/factory.ex
@@ -1,22 +1,50 @@
 defmodule GodwokenExplorer.Factory do
-  use ExMachina.Ecto, repo: Explorer.Repo
+  use ExMachina.Ecto, repo: GodwokenExplorer.Repo
 
-  alias GodwokenExplorer.Block
+  alias GodwokenExplorer.{
+    Block,
+    Transaction
+  }
 
   def block_factory do
     %Block{
       aggregator_id: 0,
       average_gas_price: nil,
-      hash: "0x4ab47c3a847ede67fdad6357e7bf0a017fdb1be3e437a675a742e268af39bc3d",
-      layer1_block_number: 3312,
+      hash:
+        sequence(:hash, &"0x#{&1}47c3a847ede67fdad6357e7bf0a017fdb1be3e437a675a742e268af39bc3d",
+          start_at: 100
+        ),
+      layer1_block_number: sequence(:layer1_block_number, & &1, start_at: 3100),
       layer1_tx_hash: "0x9d551b30032f9c4f30a931e431cc7bf91afc0d912880b90371f0a668c7af9489",
-      number: 100,
+      number: sequence(:number, & &1, start_at: 100),
       parent_hash: "0xa3c7622d9016ce604fdcda94885923fcb576927667bfe915125d190db4b2b1d2",
       size: nil,
       status: :committed,
-      timestamp: ~U[2021-02-28 13:10:49.000000Z],
-      transaction_count: 0,
+      timestamp:
+        sequence(
+          :timestamp,
+          &"#{NaiveDateTime.utc_now() |> DateTime.from_naive!("Etc/UTC") |> DateTime.add(&1)}",
+          start_at: 60
+        ),
+      transaction_count: 1,
+      transactions: build_list(1, :transaction),
       tx_fees: nil
-   }
+    }
+  end
+
+  def transaction_factory do
+    %Transaction{
+      args: "0x01000000060000001600000000000000000000000000000001000000000000000000000000000000",
+      from_account_id: 2,
+      hash:
+        sequence(:hash, &"0x#{&1}b6c0a6f68c929453197ca43b06b4735e4c04b105b9418954aae9240bfa7330",
+          start_at: 100
+        ),
+      block_number: sequence(:block_number, & &1, start_at: 100),
+      nonce: sequence(:nonce, &(&1), start_at: 0),
+      status: :committed,
+      to_account_id: 6,
+      type: :sudt
+    }
   end
 end
