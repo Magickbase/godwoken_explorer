725eeb46d3ecd8ac7a175d1d7515e7ada6dadbf0
Miles Zhang <mingchang555@hotmail.com>
2021-07-14 00:02:26 +0800

    chore: update deps
    Signed-off-by: Miles Zhang <mingchang555@hotmail.com>

--- mix.exs
+++ mix.exs
@@ -41,11 +41,11 @@ defmodule GodwokenExplorer.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.5.7"},
-      {:phoenix_ecto, "~> 4.1"},
-      {:ecto_sql, "~> 3.4"},
+      {:phoenix, "~> 1.5.9"},
+      {:phoenix_ecto, "~> 4.3"},
+      {:ecto_sql, "~> 3.6.2"},
       {:postgrex, ">= 0.0.0"},
-      {:phoenix_html, "~> 2.11"},
+      {:phoenix_html, "~> 2.14.3"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:phoenix_live_dashboard, "~> 0.4"},
       {:telemetry_metrics, "~> 0.4"},
