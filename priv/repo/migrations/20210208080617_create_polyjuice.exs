a57f896882875cca2bb59cb290b3213955fdf68d
Miles Zhang <mingchang555@hotmail.com>
2021-08-10 23:21:42 +0800

    fix: match new polyjuice struct
    Signed-off-by: Miles Zhang <mingchang555@hotmail.com>

--- priv/repo/migrations/20210208080617_create_polyjuice.exs
+++ priv/repo/migrations/20210208080617_create_polyjuice.exs
@@ -5,7 +5,6 @@ defmodule GodwokenExplorer.Repo.Migrations.CreatePolyjuice do
     create table(:polyjuice) do
       add :tx_hash, :bytea
       add :is_create, :boolean, default: false, null: false
-      add :is_static, :boolean, default: false, null: false
       add :gas_limit, :bigint
       add :gas_price, :decimal
       add :value, :decimal
