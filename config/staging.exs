ee660744a14d6a8062b859aab8ce909439eb4410
Miles Zhang <mingchang555@hotmail.com>
2021-12-03 21:59:49 +0800

    chore: add appsignal
    Signed-off-by: Miles Zhang <mingchang555@hotmail.com>

--- config/staging.exs
+++ config/staging.exs
@@ -72,3 +72,5 @@ config :logger,
 # Finally import the config/prod.secret.exs which loads secrets
 # and configuration from environment variables.
 import_config "staging.secret.exs"
+
+config :appsignal, :config, active: false
