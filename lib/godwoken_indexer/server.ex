05bd8f2919686e7813eb27e80f833dbab7631b36
Miles Zhang <mingchang555@hotmail.com>
2021-12-06 00:01:02 +0800

    feat: remove receipt worker
    Signed-off-by: Miles Zhang <mingchang555@hotmail.com>

--- lib/godwoken_indexer/server.ex
+++ lib/godwoken_indexer/server.ex
@@ -9,7 +9,6 @@ defmodule GodwokenIndexer.Server do
     children = [
       GodwokenIndexer.Block.Supervisor,
       GodwokenIndexer.Account.SyncSupervisor,
-      GodwokenIndexer.Transaction.Supervisor
     ]
 
     Supervisor.init(children, strategy: :one_for_one)
