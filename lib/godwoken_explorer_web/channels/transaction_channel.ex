ccbdfbc63cf51bee4a3b6239c5f61d621e79ccfd
Miles Zhang <mingchang555@hotmail.com>
2021-12-06 09:58:24 +0800

    feat: pending tx dont join channel
    Signed-off-by: Miles Zhang <mingchang555@hotmail.com>

--- lib/godwoken_explorer_web/channels/transaction_channel.ex
+++ lib/godwoken_explorer_web/channels/transaction_channel.ex
@@ -6,30 +6,34 @@ defmodule GodwokenExplorerWeb.TransactionChannel do
 
   import GodwokenRPC.Util, only: [stringify_and_unix_maps: 1]
 
-  alias GodwokenExplorer.Transaction
+  alias GodwokenExplorer.{Transaction, PendingTransaction}
 
   intercept(["refresh"])
 
   def join("transactions:" <> tx_hash, _params, socket) do
     tx = Transaction.find_by_hash(tx_hash)
 
-    result =
-      stringify_and_unix_maps(%{
-          hash: tx.hash,
-          timestamp: tx.timestamp,
-          finalize_state: tx.status,
-          l2_block: tx.l2_block_number,
-          l1_block: tx.l1_block_number,
-          from: tx.from,
-          to: tx.to,
-          nonce: tx.nonce,
-          args: tx.args,
-          type: tx.type,
-          gas_price: tx |> Map.get(:gas_price, Decimal.new(0)),
-          fee: tx |> Map.get(:fee, Decimal.new(0))
-      })
+    if is_nil(tx) do
+      {:error, %{reason: "may be a pending tx"}}
+    else
+      result =
+        stringify_and_unix_maps(%{
+            hash: tx.hash,
+            timestamp: tx.timestamp,
+            finalize_state: tx.status,
+            l2_block: tx.l2_block_number,
+            l1_block: tx.l1_block_number,
+            from: tx.from,
+            to: tx.to,
+            nonce: tx.nonce,
+            args: tx.args,
+            type: tx.type,
+            gas_price: tx |> Map.get(:gas_price, Decimal.new(0)),
+            fee: tx |> Map.get(:fee, Decimal.new(0))
+        })
 
-    {:ok, result, assign(socket, :tx_hash, tx_hash)}
+      {:ok, result, assign(socket, :tx_hash, tx_hash)}
+    end
   end
 
   def handle_out(
