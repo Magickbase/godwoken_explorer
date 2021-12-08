06f2af5b3a54cf62a813440fb0d9af512648b6a2
Miles Zhang <mingchang555@hotmail.com>
2021-12-08 11:51:56 +0800

    feat: adjust udt admin
    Signed-off-by: Miles Zhang <mingchang555@hotmail.com>

---
+++ priv/repo/migrations/20211208022922_add_type_to_udts.exs
@@ -0,0 +1,12 @@
+defmodule GodwokenExplorer.Repo.Migrations.AddTypeToUdts do
+  use Ecto.Migration
+
+  def change do
+    alter table(:udts) do
+      add :value, :decimal
+      add :description, :string
+      add :official_site, :string
+      add :type, :string, default: "bridge"
+    end
+  end
+end
