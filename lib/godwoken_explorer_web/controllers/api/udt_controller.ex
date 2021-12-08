5f235c4a45854d9589071c3a1811382ee7108cc8
Miles Zhang <mingchang555@hotmail.com>
2021-12-08 17:03:43 +0800

    feat: add udt show api
    Signed-off-by: Miles Zhang <mingchang555@hotmail.com>

--- lib/godwoken_explorer_web/controllers/api/udt_controller.ex
+++ lib/godwoken_explorer_web/controllers/api/udt_controller.ex
@@ -1,12 +1,26 @@
 defmodule GodwokenExplorerWeb.API.UDTController do
   use GodwokenExplorerWeb, :controller
 
-  alias GodwokenExplorer.UDTView
+  alias GodwokenExplorer.{UDT, UDTView, Repo}
+
+  plug JSONAPI.QueryParser, view: UDTView
 
   # fields[udt]=id,name,symbol,supply,holders,type,short_address
   def index(conn, params) do
     results = UDTView.list(conn.assigns[:page] || 1)
     data = JSONAPI.Serializer.serialize(UDTView, results.entries, conn, %{total_page: results.total_pages, current_page: results.page_number} )
     json(conn, data)
+  end
+
+  def show(conn, %{"id" => id} = params) do
+    case Repo.get(UDT, id) do
+      nil -> %{
+        error_code: 404,
+        message: "not found"
+      }
+      udt = %UDT{} ->
+        data = JSONAPI.Serializer.serialize(UDTView, udt, conn)
+        json(conn, data)
+    end
   end
 end
