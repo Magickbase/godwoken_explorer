a7bf252d99327b6aab6a433a0a5023ea416b0933
Miles Zhang <mingchang555@hotmail.com>
2021-12-09 11:23:02 +0800

    feat: add contract methods
    Signed-off-by: Miles Zhang <mingchang555@hotmail.com>

---
+++ priv/repo/migrations/20211209014712_create_contract_methods.exs
@@ -0,0 +1,15 @@
+defmodule GodwokenExplorer.Repo.Migrations.CreateContractMethods do
+  use Ecto.Migration
+
+  def change do
+    create table(:contract_methods) do
+      add :identifier, :integer, null: false
+      add :abi, :map, null: false
+      add :type, :string, null: false
+
+      timestamps()
+    end
+
+    create(unique_index(:contract_methods, [:identifier, :abi]))
+  end
+end
