3527a9bce945f80f0adb0bd8bd33fe2f0c9d7ffe
Miles Zhang <mingchang555@hotmail.com>
2021-12-09 15:12:17 +0800

    feat: return transfer detail
    Signed-off-by: Miles Zhang <mingchang555@hotmail.com>

--- lib/godwoken_explorer/polyjuice.ex
+++ lib/godwoken_explorer/polyjuice.ex
@@ -4,6 +4,10 @@ defmodule GodwokenExplorer.Polyjuice do
 
   import Ecto.Changeset
 
+  require Logger
+
+  alias ABI.FunctionSelector
+
   schema "polyjuice" do
     field :is_create, :boolean, default: false
     field :gas_limit, :integer
@@ -31,19 +35,20 @@ defmodule GodwokenExplorer.Polyjuice do
   end
 
   def create_polyjuice(attrs) do
-    gas_used = retry with: constant_backoff(500) |> Stream.take(3) do
-      case GodwokenRPC.fetch_receipt(attrs[:hash]) do
-        {:ok, gas_used} ->
-          gas_used
+    gas_used =
+      retry with: constant_backoff(500) |> Stream.take(3) do
+        case GodwokenRPC.fetch_receipt(attrs[:hash]) do
+          {:ok, gas_used} ->
+            gas_used
 
-        {:error, _} ->
-          {:error, :node_error}
+          {:error, _} ->
+            {:error, :node_error}
+        end
+      after
+        result -> result
+      else
+        _error -> nil
       end
-    after
-      result -> result
-    else
-      _error -> nil
-    end
 
     %Polyjuice{}
     |> Polyjuice.changeset(attrs)
@@ -51,4 +56,103 @@ defmodule GodwokenExplorer.Polyjuice do
     |> Ecto.Changeset.put_change(:gas_used, gas_used)
     |> Repo.insert()
   end
+
+  def get_method_name(transaction) do
+    case decode_input(transaction) do
+      {:ok, _, decoded_func, _} ->
+        parse_method_name(decoded_func)
+      nil ->
+        nil
+    end
+  end
+
+  def merge_transfer_args(record) do
+    case decode_input(record) do
+      {:ok, method_sign, method_desc, args} ->
+        method_name = parse_method_name(method_desc)
+        if method_name == "Transfer" do
+          {address, transfer_count} = Polyjuice.parse_transfer_args(args)
+          record |> Map.merge(%{to: "0x" <> address, contract_address: record.to, transfer_count: transfer_count})
+        else
+          record
+        end
+      _ -> record
+    end
+  end
+
+  def decode_input(transaction) do
+    with %Transaction{to_account_id: to_account_id, hash: hash} <-
+           Repo.get_by(Transaction, hash: transaction.hash),
+         %SmartContract{abi: full_abi} <- Repo.get_by(SmartContract, account_id: to_account_id) do
+      do_decoded_input_data(
+        Base.decode16!(String.slice(transaction.input, 2..-1), case: :lower),
+        full_abi,
+        hash
+      )
+    else
+      _ -> nil
+    end
+  end
+
+  def parse_transfer_args(args) do
+    {args |> List.first() |> elem(2) |> Base.encode16(), args |> List.last() |> elem(2)}
+  end
+
+  #  {:ok, "a9059cbb", "transfer(address _to, uint256 _value)",
+  #  [
+  #    {"_to", "address",
+  #      <<84, 194, 168, 114, 226, 215, 140, 253, 44, 95, 160, 152, 61, 61, 77, 139,
+  #        251, 26, 202, 98>>},
+  #    {"_value", "uint256", 1000000000}
+  #  ]}
+  defp do_decoded_input_data(data, full_abi, hash) do
+    with {:ok, {selector, values}} <- find_and_decode(full_abi, data, hash),
+         {:ok, mapping} <- selector_mapping(selector, values, hash),
+         identifier <- Base.encode16(selector.method_id, case: :lower),
+         text <- function_call(selector.function, mapping),
+         do: {:ok, identifier, text, mapping}
+  end
+
+  defp function_call(name, mapping) do
+    text =
+      mapping
+      |> Stream.map(fn {name, type, _} -> [type, " ", name] end)
+      |> Enum.intersperse(", ")
+
+    IO.iodata_to_binary([name, "(", text, ")"])
+  end
+
+  defp find_and_decode(abi, data, hash) do
+    result =
+      abi
+      |> ABI.parse_specification()
+      |> ABI.find_and_decode(data)
+
+    {:ok, result}
+  rescue
+    _ ->
+      Logger.warn(fn -> ["Could not decode input data for transaction: ", hash] end)
+      {:error, :could_not_decode}
+  end
+
+  defp selector_mapping(selector, values, hash) do
+    types = Enum.map(selector.types, &FunctionSelector.encode_type/1)
+
+    mapping = Enum.zip([selector.input_names, types, values])
+
+    {:ok, mapping}
+  rescue
+    _ ->
+      Logger.warn(fn -> ["Could not decode input data for transaction: ", hash] end)
+      {:error, :could_not_decode}
+  end
+
+  def parse_method_name(method_desc) do
+    method_desc
+    |> String.split("(")
+    |> Enum.at(0)
+    |> upcase_first
+  end
+
+  defp upcase_first(<<first::utf8, rest::binary>>), do: String.upcase(<<first::utf8>>) <> rest
 end
