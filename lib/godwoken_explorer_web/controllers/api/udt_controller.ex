2b7dd30573fc0467ceddfcae315258d615cc87da
Miles Zhang <mingchang555@hotmail.com>
2021-12-10 17:02:56 +0800

    fix: use conn.params get page
    Signed-off-by: Miles Zhang <mingchang555@hotmail.com>

--- lib/godwoken_explorer_web/controllers/api/udt_controller.ex
+++ lib/godwoken_explorer_web/controllers/api/udt_controller.ex
@@ -7,7 +7,7 @@ defmodule GodwokenExplorerWeb.API.UDTController do
 
   # fields[udt]=id,name,symbol,supply,holders,type,short_address
   def index(conn, _params) do
-    results = UDTView.list(conn.assigns[:page] || 1)
+    results = UDTView.list(conn.params["page"] || 1)
     data = JSONAPI.Serializer.serialize(UDTView, results.entries, conn, %{total_page: results.total_pages, current_page: results.page_number} )
     json(conn, data)
   end
--- lib/godwoken_explorer_web/controllers/api/withdrawal_history_controller.ex
+++ lib/godwoken_explorer_web/controllers/api/withdrawal_history_controller.ex
@@ -4,7 +4,7 @@ defmodule GodwokenExplorerWeb.API.WithdrawalHistoryController do
   alias GodwokenExplorer.WithdrawalHistoryView
 
   def index(conn, %{"owner_lock_hash" => "0x" <> _} = params) do
-    results = WithdrawalHistoryView.find_by_owner_lock_hash(String.downcase(params["owner_lock_hash"]), conn.assigns[:page] || 1)
+    results = WithdrawalHistoryView.find_by_owner_lock_hash(String.downcase(params["owner_lock_hash"]), conn.params["page"] || 1)
     data = JSONAPI.Serializer.serialize(WithdrawalHistoryView, results.entries, conn, %{total_page: results.total_pages, current_page: results.page_number} )
     json(conn, data)
   end
