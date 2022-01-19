3770bb6f9d39a32f942834c8f26f7204e2bd04b8
Miles Zhang <mingchang555@hotmail.com>
2022-01-19 16:58:47 +0800

    fix: return error when account not exist
    Signed-off-by: Miles Zhang <mingchang555@hotmail.com>

--- lib/godwoken_explorer_web/views/error_view.ex
+++ lib/godwoken_explorer_web/views/error_view.ex
@@ -10,7 +10,15 @@ defmodule GodwokenExplorerWeb.ErrorView do
   # By default, Phoenix returns the status message from
   # the template name. For example, "404.html" becomes
   # "Not Found".
-  def template_not_found(template, _assigns) do
-    Phoenix.Controller.status_message_from_template(template)
+  def render("404.json", _assigns) do
+    # When encoded, the changeset returns its errors
+    # as a JSON object. So we just pass it forward.
+    %{
+      errors:  %{
+        status: "404",
+        title: "not found",
+        detail: ""
+      }
+    }
   end
 end
--- test/godwoken_explorer_web/views/error_view_test.exs
+++ test/godwoken_explorer_web/views/error_view_test.exs
@@ -4,11 +4,14 @@ defmodule GodwokenExplorerWeb.ErrorViewTest do
   # Bring render/3 and render_to_string/3 for testing custom views
   import Phoenix.View
 
-  test "renders 404.html" do
-    assert render_to_string(GodwokenExplorerWeb.ErrorView, "404.html", []) == "Not Found"
-  end
-
-  test "renders 500.html" do
-    assert render_to_string(GodwokenExplorerWeb.ErrorView, "500.html", []) == "Internal Server Error"
+  test "renders 404.json" do
+    assert render(GodwokenExplorerWeb.ErrorView, "404.json", %{title: "account not found", detail: ""}) ==
+      %{
+        errors:  %{
+          status: "404",
+          title: "not found",
+          detail: ""
+        }
+      }
   end
 end
