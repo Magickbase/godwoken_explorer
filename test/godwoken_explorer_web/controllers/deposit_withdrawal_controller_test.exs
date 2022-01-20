25de892181a28853c30370c8b92f51de48f32589
Miles Zhang <mingchang555@hotmail.com>
2022-01-20 11:23:45 +0800

    feat: deposit and withdrawal list by block_number or udt_id
    Signed-off-by: Miles Zhang <mingchang555@hotmail.com>

---
+++ test/godwoken_explorer_web/controllers/deposit_withdrawal_controller_test.exs
@@ -0,0 +1,178 @@
+defmodule GodwokenExplorerWeb.DepositWithdrawalControllerTest do
+  use GodwokenExplorerWeb.ConnCase
+  use GodwokenExplorer, :schema
+
+  setup do
+    UDT.find_or_create_by(%{
+      id: 1,
+      name: "CKB",
+      decimal: 8,
+      script_hash: "0x0000000000000000000000000000000000000000000000000000000000000000",
+      type: :bridge
+    })
+
+    Block.create_block(%{
+      hash: "0x9e449451846827df40c9a8bcb2809256011afbbf394de676d52535c3ca32a518",
+      parent_hash: "0xa04ecc2bb1bc634848535b60b3223c1cd5278aa93abb2c138687da8ffa9ffd48",
+      number: 68738,
+      timestamp: ~U[2021-10-31 05:39:38.000000Z],
+      status: :finalized,
+      aggregator_id: 0,
+      transaction_count: 1
+    })
+
+    Repo.insert(%Account{
+      id: 468,
+      nonce: 90,
+      script: %{
+        "args" =>
+          "0x40d73f0d3c561fcaae330eabc030d8d96a9d0af36d0c5114883658a350cb9e3b085a61d7164735fc5378e590b5ed1448561e1a48",
+        "code_hash" => "0x1563080d175bf8ddd44a48e850cecf0c0b4575835756eb5ffd53ad830931b9f9",
+        "hash_type" => "type"
+      },
+      script_hash: "0xfa2ae9de22bbca35fc44f20efe7a3d2789556d4c50a7c2b4e460269f13b77c58",
+      short_address: "0xfa2ae9de22bbca35fc44f20efe7a3d2789556d4c",
+      eth_address: "0x085a61d7164735fc5378e590b5ed1448561e1a48",
+      type: :user
+    })
+
+    WithdrawalRequest.create_withdrawal_request(%{
+      account_script_hash: "0xfa2ae9de22bbca35fc44f20efe7a3d2789556d4c50a7c2b4e460269f13b77c58",
+      amount: D.new(0),
+      block_hash: "0x07aafde68ea70169bb54cf76b44496d8f5deba5ac89cb1ddc20d10646ddfc09f",
+      block_number: 68738,
+      capacity: D.new(383_220_966_182),
+      fee_amount: D.new(0),
+      fee_udt_id: 1,
+      nonce: 59,
+      owner_lock_hash: "0x66db0f8f6b0ac8b4e92fdfcef8d04a3251a118ccae0ff436957e2c646f083ebd",
+      payment_lock_hash: "0x0000000000000000000000000000000000000000000000000000000000000000",
+      sell_amount: D.new(0),
+      sell_capacity: D.new(0),
+      sudt_script_hash: "0x0000000000000000000000000000000000000000000000000000000000000000",
+      udt_id: 1
+    })
+
+    DepositHistory.create_or_update_history!(%{
+      amount: D.new(40_000_000_000),
+      ckb_lock_hash: "0xe6c7befcbf4697f1a7f8f04ffb8de71f5304826af7bfce3e4d396483e935820a",
+      layer1_block_number: 5_744_914,
+      layer1_output_index: 0,
+      layer1_tx_hash: "0x41876f5c3ea0d96219c42ea5b4e6cedba61c59fa39bf163765a302f6e43c3847",
+      script_hash: "0xfa2ae9de22bbca35fc44f20efe7a3d2789556d4c50a7c2b4e460269f13b77c58",
+      timestamp: ~U[2021-12-02 22:39:39.585000Z],
+      udt_id: 1
+    })
+
+    :ok
+  end
+
+  describe "index" do
+    test "lists by block_number", %{conn: conn} do
+      conn =
+        get(
+          conn,
+          Routes.deposit_withdrawal_path(conn, :index, block_number: "68738")
+        )
+
+      assert json_response(conn, 200) ==
+               %{
+                 "data" => [
+                   %{
+                     "account_script_hash" =>
+                       "0xfa2ae9de22bbca35fc44f20efe7a3d2789556d4c50a7c2b4e460269f13b77c58",
+                     "block_hash" =>
+                       "0x07aafde68ea70169bb54cf76b44496d8f5deba5ac89cb1ddc20d10646ddfc09f",
+                     "block_number" => 68738,
+                     "nonce" => 59,
+                     "owner_lock_hash" =>
+                       "0x66db0f8f6b0ac8b4e92fdfcef8d04a3251a118ccae0ff436957e2c646f083ebd",
+                     "payment_lock_hash" =>
+                       "0x0000000000000000000000000000000000000000000000000000000000000000",
+                     "sudt_script_hash" =>
+                       "0x0000000000000000000000000000000000000000000000000000000000000000",
+                     "udt_id" => 1,
+                     "value" => "0.0000000000000000000000000000",
+                     "capacity" => "383220966182",
+                     "fee_value" => "0.0000000000000000000000000000",
+                     "fee_udt_icon" => nil,
+                     "fee_udt_id" => 1,
+                     "fee_udt_name" => "CKB",
+                     "fee_udt_symbol" => nil,
+                     "sell_value" => "0.0000000000000000000000000000",
+                     "sell_capacity" => "0",
+                     "timestamp" => "2021-10-31T05:39:38.000000Z",
+                     "type" => "withdrawal",
+                     "udt_icon" => nil,
+                     "udt_name" => "CKB",
+                     "udt_symbol" => nil
+                   }
+                 ],
+                 "page" => "1",
+                 "total_count" => "1"
+               }
+    end
+
+    test "lists by udt_id", %{conn: conn} do
+      conn =
+        get(
+          conn,
+          Routes.deposit_withdrawal_path(conn, :index, udt_id: "1")
+        )
+
+      assert json_response(conn, 200) ==
+               %{
+                 "data" => [
+                   %{
+                     "account_script_hash" =>
+                       "0xfa2ae9de22bbca35fc44f20efe7a3d2789556d4c50a7c2b4e460269f13b77c58",
+                     "block_hash" =>
+                       "0x07aafde68ea70169bb54cf76b44496d8f5deba5ac89cb1ddc20d10646ddfc09f",
+                     "block_number" => 68738,
+                     "capacity" => "383220966182",
+                     "fee_udt_icon" => nil,
+                     "fee_udt_id" => 1,
+                     "fee_udt_name" => "CKB",
+                     "fee_udt_symbol" => nil,
+                     "fee_value" => "0.0000000000000000000000000000",
+                     "nonce" => 59,
+                     "owner_lock_hash" =>
+                       "0x66db0f8f6b0ac8b4e92fdfcef8d04a3251a118ccae0ff436957e2c646f083ebd",
+                     "payment_lock_hash" =>
+                       "0x0000000000000000000000000000000000000000000000000000000000000000",
+                     "sell_capacity" => "0",
+                     "sell_value" => "0.0000000000000000000000000000",
+                     "sudt_script_hash" =>
+                       "0x0000000000000000000000000000000000000000000000000000000000000000",
+                     "timestamp" => "2021-10-31T05:39:38.000000Z",
+                     "type" => "withdrawal",
+                     "udt_icon" => nil,
+                     "udt_id" => 1,
+                     "udt_name" => "CKB",
+                     "udt_symbol" => nil,
+                     "value" => "0.0000000000000000000000000000"
+                   },
+                   %{
+                     "ckb_lock_hash" =>
+                       "0xe6c7befcbf4697f1a7f8f04ffb8de71f5304826af7bfce3e4d396483e935820a",
+                     "layer1_block_number" => 5_744_914,
+                     "layer1_output_index" => 0,
+                     "layer1_tx_hash" =>
+                       "0x41876f5c3ea0d96219c42ea5b4e6cedba61c59fa39bf163765a302f6e43c3847",
+                     "script_hash" =>
+                       "0xfa2ae9de22bbca35fc44f20efe7a3d2789556d4c50a7c2b4e460269f13b77c58",
+                     "timestamp" => "2021-12-02T22:39:39.585000Z",
+                     "type" => "deposit",
+                     "udt_icon" => nil,
+                     "udt_id" => 1,
+                     "udt_name" => "CKB",
+                     "udt_symbol" => nil,
+                     "value" => "400.0000000000000000"
+                   }
+                 ],
+                 "page" => "1",
+                 "total_count" => "2"
+               }
+    end
+  end
+end
