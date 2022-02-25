a930d8442a3a3a819baf7469291d29c9ecc97d15
Naupio <naupio@163.com>
2022-02-25 10:19:42 +0800

    (fix) fix typo

--- lib/godwoken_explorer/scheme_util.ex
+++
@@ -1,9 +0,0 @@
-defmodule GodwokenExploree.SchemeUtils do
-  def base_fields_without_id(mod) do
-    mod.__schema__(:fields) -- [:id, :inserted_at, :updated_at]
-  end
-
-  def base_fields(mod) do
-    mod.__schema__(:fields) -- [:inserted_at, :updated_at]
-  end
-end
