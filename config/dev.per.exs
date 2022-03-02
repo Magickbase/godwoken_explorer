d87ada048875e8c26b336924e9e51650cc34f34e
Miles Zhang <mingchang555@hotmail.com>
2022-03-02 16:44:02 +0800

    chore: update dev configuration
    Signed-off-by: Miles Zhang <mingchang555@hotmail.com>

--- config/dev.per.exs
+++ config/dev.per.exs
@@ -1,29 +1,30 @@
 import Config
 
+
 config :godwoken_explorer,
-  meta_contract_code_hash: "0xd6d4b52ed2882cb0b2e1f802817c0ab05bd27da89a41ceddb25e0e347689ce69",
-  udt_code_hash: "0xb6d6a2882d3d08cea565047bfe901cb2afe0cb790ea5e1b61e0532ef237c4a02",
+  meta_contract_code_hash: "0x4a8ef5f6b366cb84e362dc747c314e32d5e2f1a6f64d118eaec6df2c36512ac0",
+  udt_code_hash: "0x4e55cb08c3c772414e364aa5ed26609cfd3ee6619a2bb78c3bafa6f2f41b16b3",
   polyjuice_validator_code_hash:
-    "0xbeb77e49c6506182ec0c02546aee9908aafc1561ec13beb488d14184c6cd1b79",
-  layer2_lock_code_hash: "0xdeec13a7b8e100579541384ccaf4b5223733e4a5483c3aec95ddc4c1d5ea5b22",
-  eoa_code_hash: "0x28380fadb43a6f139d61a2509b69ecd2fbb2f61847ef6d39371b4f906c151ab5",
-  rollup_script_hash: "0x4cc2e6526204ae6a2e8fcf12f7ad472f41a1606d5b9624beebd215d780809f6a",
+    "0x848753a2b16c63682c7de4e6cc7167890ff3f821d61328f45386d91ece54373e",
+  eth_eoa_type_hash: "0x6ac8027edfd86557a0b02fb8b9dce9ffc2bb2ac0b2f0352f74912bb546dc374c",
+  tron_eoa_type_hash: "0xa289d7e6f46ae922a57691b42ce7b8ff9387f5d18ccce0287e14916a03cae51c",
+  rollup_script_hash: "0xbd3f6ca6bdc273f1699e67a7b72cae0e0a7250646f168d03adab3892b5e2cfef",
   rollup_cell_type: %{
-    code_hash: "0x5c365147bb6c40e817a2a53e0dec3661f7390cc77f0c02db138303177b12e9fb",
+    code_hash: "0x3949a52e86048d6184641e7e441c543097559829daa9f60b7ee137031123ef24",
     hash_type: "type",
-    args: "0x213743d13048e9f36728c547ab736023a7426e15a3d7d1c82f43ec3b5f266df2"
+    args: "0xd35def1737c65ef34969c6cc93d5b87841434024e15d58693ea054059b58fc8d"
   },
   deposition_lock: %{
-    code_hash: "0x5a2506bb68d81a11dcadad4cb7eae62a17c43c619fe47ac8037bc8ce2dd90360",
+    code_hash: "0x6f46d7c451d63e584e60e6a748662df044e4ada30291e0d35aa1d0ddb1237f40",
     hash_type: "type",
-    args: "0x4cc2e6526204ae6a2e8fcf12f7ad472f41a1606d5b9624beebd215d780809f6a"
+    args: "0xbd3f6ca6bdc273f1699e67a7b72cae0e0a7250646f168d03adab3892b5e2cfef"
   },
   withdrawal_lock: %{
-    code_hash: "0x170ef156e9f6132dbca6069dfd3e436f7d91c29d3ac7332c4b33e633b6a299b5",
+    code_hash: "0xc897744bffee22bae91afdf581e396d5d1f34313d2aa1b3fa8121e561f2bae59",
     hash_type: "type",
-    args: "0x4cc2e6526204ae6a2e8fcf12f7ad472f41a1606d5b9624beebd215d780809f6a"
+    args: "0xbd3f6ca6bdc273f1699e67a7b72cae0e0a7250646f168d03adab3892b5e2cfef"
   },
-  init_godwoken_l1_block_number: 2_286_200,
+  init_godwoken_l1_block_number: 4_499_838,
   ckb_token_script_hash: "0x0000000000000000000000000000000000000000000000000000000000000000",
   eth_token_script_hash: "0xa9eb9df467715766b009ad57cf4c7a2977bc8377d51ace37a3653f3bbb540b7c"
 
@@ -32,18 +33,17 @@ config :godwoken_explorer, :basic_auth, username: "hello", password: "secret"
 config :godwoken_explorer, GodwokenExplorer.Repo,
   username: "postgres",
   password: "password",
-  database: "godwoken_explorer_dev",
+  database: "godwoken_explorer_dev_v1",
   hostname: "localhost",
   show_sensitive_data_on_connection_error: true,
-  pool_size: 10,
-  ownership_timeout: 60_000,
+  pool_size: 20,
   timeout: 60_000
 
 config :godwoken_explorer,
   # only read node
   json_rpc_named_arguments: [
     http: GodwokenRPC.HTTP.HTTPoison,
-    url: "http://localhost:8119",
+    url: "https://godwoken-testnet-web3-v1-rpc.ckbapp.dev",
     http_options: [
       recv_timeout: :timer.minutes(10),
       timeout: :timer.minutes(10)
@@ -52,7 +52,7 @@ config :godwoken_explorer,
   # mempool node
   mempool_rpc_named_arguments: [
     http: GodwokenRPC.HTTP.HTTPoison,
-    url: "http://localhost:8119",
+    url: "https://godwoken-testnet-web3-v1-rpc.ckbapp.dev",
     http_options: [
       recv_timeout: :timer.minutes(10),
       timeout: :timer.minutes(10)
@@ -60,7 +60,7 @@ config :godwoken_explorer,
   ],
   ckb_indexer_named_arguments: [
     http: GodwokenRPC.HTTP.HTTPoison,
-    url: "http://localhost:8116",
+    url: "http://116.62.22.144:8116",
     http_options: [
       recv_timeout: :timer.minutes(10),
       timeout: :timer.minutes(10)
@@ -68,7 +68,7 @@ config :godwoken_explorer,
   ],
   ckb_rpc_named_arguments: [
     http: GodwokenRPC.HTTP.HTTPoison,
-    url: "http://localhost:8114",
+    url: "http://116.62.22.144:8114",
     http_options: [
       recv_timeout: :timer.minutes(10),
       timeout: :timer.minutes(10)
