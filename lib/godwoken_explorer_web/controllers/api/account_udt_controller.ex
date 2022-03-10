7c67ba8a56856b4825e5b01bedb0db4f9904fb37
Miles Zhang <mingchang555@hotmail.com>
2022-03-10 13:27:42 +0800

    feat: sort udt holder list
    Signed-off-by: Miles Zhang <mingchang555@hotmail.com>

---
+++ lib/godwoken_explorer_web/controllers/api/account_udt_controller.ex
@@ -0,0 +1,25 @@
+defmodule GodwokenExplorerWeb.API.AccountUDTController do
+  use GodwokenExplorerWeb, :controller
+
+  action_fallback GodwokenExplorerWeb.API.FallbackController
+
+  alias GodwokenExplorer.{Repo, AccountUDT, UDT}
+
+  def index(conn, %{"udt_id" => udt_id}) do
+    with %UDT{} <- Repo.get(UDT, udt_id) do
+      result =
+        AccountUDT.sort_holder_list(
+          udt_id,
+          %{page: conn.params["page"] || 1, page_size: conn.assigns.page_size}
+        )
+
+      json(
+        conn,
+        result
+      )
+    else
+      _ ->
+        {:error, :not_found}
+    end
+  end
+end
