2193ca715f2708cc0085e90d7fca35b3d84b2f16
Miles Zhang <mingchang555@hotmail.com>
2022-03-14 16:00:35 +0800

    feat: add unknown account type
    Signed-off-by: Miles Zhang <mingchang555@hotmail.com>

--- config/dev.per.exs
+++ config/dev.per.exs
@@ -1,6 +1,5 @@
 import Config
 
-
 config :godwoken_explorer,
   rollup_type_hash: "0x4940246f168f4106429dc641add3381a44b5eef61e7754142f594e986671a575",
   meta_contract_validator_type_hash: "0xe37425948da964046bc470a77f5b3df3a661e8b0e0f8c1987239d9e4b9a629f5",
@@ -9,6 +8,7 @@ config :godwoken_explorer,
     "0x8755bcc380e3494b6a2ca9657d16fd2254f7570731c4b87867ed8b747b1b3457",
   eth_eoa_type_hash: "0x10571f91073fdc3cdef4ddad96b4204dd30d6355f3dda9a6d7fc0fa0326408da",
   tron_eoa_type_hash: "0x7e19e979f77305cdd61e39258b747809297ece6ab4d579ee38de8bce85d52124",
+  eth_addr_reg_validator_script_type_hash: "0xea308236e06a55ea8951de922eb9eb7344ec8ac7e1909a29d3b580b97a980a4f",
   rollup_cell_type: %{
     code_hash: "0x0d3bfeaa292a59fcb58ed026e8f14e2167bd27f1765aa4b2af7d842b6123c6a9",
     hash_type: "type",
