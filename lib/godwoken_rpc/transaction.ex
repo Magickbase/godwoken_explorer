0629e223183c44feff5dd1499e2523a70403cb0e
Miles Zhang <mingchang555@hotmail.com>
2022-03-14 12:05:32 +0800

    feat: parse transaction
    Signed-off-by: Miles Zhang <mingchang555@hotmail.com>

--- lib/godwoken_rpc/transaction.ex
+++ lib/godwoken_rpc/transaction.ex
@@ -48,77 +48,67 @@ defmodule GodwokenRPC.Transaction do
         "block_number" => block_number,
         "raw" => %{
           "from_id" => from_account_id,
-          "to_id" => to_account_id,
-          "nonce" => nonce,
-          "args" => "0x" <> args
-        },
-        "hash" => hash
-      })
-      when to_account_id == @creator_id do
-    from_account_id = hex_to_number(from_account_id)
-
-    case parse_eth_address_registry_args(args) do
-      {"EthToGw", eth_address, _} ->
-        Logger.info("===========ETHToGw#{eth_address}")
-
-      {"GwToEth", gw_script_hash, _} ->
-        Logger.info("===========GwToEth#{gw_script_hash}")
-
-      {"SetMapping", gw_script_hash, fee} ->
-        Logger.info("===========SetMapping#{gw_script_hash}#{fee}")
-
-      {"BatchSetMapping", gw_script_hashes, fee} ->
-        Logger.info("===========BatchSetMapping#{gw_script_hashes}#{fee}")
-        {hashes_count, str_hashes} = gw_script_hashes |> String.split_at(8)
-        hashes = for <<x::binary-64 <- str_hashes>>, do: x
-    end
-
-    %{
-      type: :eth_address_registry,
-      hash: hash,
-      block_hash: block_hash,
-      block_number: block_number,
-      nonce: hex_to_number(nonce),
-      args: "0x" <> args,
-      from_account_id: from_account_id,
-      to_account_id: 4,
-      account_ids: [from_account_id]
-    }
-  end
-
-  def elixir_to_params(%{
-        "block_hash" => block_hash,
-        "block_number" => block_number,
-        "raw" => %{
-          "from_id" => from_account_id,
           "to_id" => to_id,
           "nonce" => nonce,
           "args" => "0x" <> args
         },
         "hash" => hash
       }) do
-    if String.starts_with?(args, "ffffff504f4c59") do
-      [is_create, gas_limit, gas_price, value, input_size, input] = parse_polyjuice_args(args)
-      from_account_id = hex_to_number(from_account_id)
-      to_account_id = hex_to_number(to_id)
+    cond do
+      String.starts_with?(args, "ffffff504f4c59") ->
+        [is_create, gas_limit, gas_price, value, input_size, input] = parse_polyjuice_args(args)
+        from_account_id = hex_to_number(from_account_id)
+        to_account_id = hex_to_number(to_id)
 
-      %{
-        type: :polyjuice,
-        hash: hash,
-        block_hash: block_hash,
-        block_number: block_number,
-        nonce: hex_to_number(nonce),
-        args: "0x" <> args,
-        from_account_id: from_account_id,
-        to_account_id: to_account_id,
-        is_create: is_create,
-        gas_limit: gas_limit,
-        gas_price: gas_price,
-        value: value,
-        input_size: input_size,
-        input: input,
-        account_ids: [from_account_id, to_account_id]
-      }
+        %{
+          type: :polyjuice,
+          hash: hash,
+          block_hash: block_hash,
+          block_number: block_number,
+          nonce: hex_to_number(nonce),
+          args: "0x" <> args,
+          from_account_id: from_account_id,
+          to_account_id: to_account_id,
+          is_create: is_create,
+          gas_limit: gas_limit,
+          gas_price: gas_price,
+          value: value,
+          input_size: input_size,
+          input: input,
+          account_ids: [from_account_id, to_account_id]
+        }
+
+      to_id == @creator_id ->
+        from_account_id = hex_to_number(from_account_id)
+        to_account_id = hex_to_number(to_id)
+
+        case parse_eth_address_registry_args(args) do
+          {"EthToGw", eth_address, _} ->
+            Logger.info("===========ETHToGw#{eth_address}")
+
+          {"GwToEth", gw_script_hash, _} ->
+            Logger.info("===========GwToEth#{gw_script_hash}")
+
+          {"SetMapping", gw_script_hash, fee} ->
+            Logger.info("===========SetMapping#{gw_script_hash}#{fee}")
+
+          {"BatchSetMapping", gw_script_hashes, fee} ->
+            Logger.info("===========BatchSetMapping#{gw_script_hashes}#{fee}")
+            {hashes_count, str_hashes} = gw_script_hashes |> String.split_at(8)
+            hashes = for <<x::binary-64 <- str_hashes>>, do: x
+        end
+
+        %{
+          type: :eth_address_registry,
+          hash: hash,
+          block_hash: block_hash,
+          block_number: block_number,
+          nonce: hex_to_number(nonce),
+          args: "0x" <> args,
+          from_account_id: from_account_id,
+          to_account_id: to_account_id,
+          account_ids: [from_account_id, to_account_id]
+        }
     end
   end
 end
