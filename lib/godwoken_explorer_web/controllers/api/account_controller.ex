41d84879874595f8494c0b1cce00575413b46d46
Miles Zhang <mingchang555@hotmail.com>
2022-03-30 17:09:04 +0800

    feat: fetch token_transfer_count and transaction_count when visit account api
    Signed-off-by: Miles Zhang <mingchang555@hotmail.com>

--- lib/godwoken_explorer_web/controllers/api/account_controller.ex
+++ lib/godwoken_explorer_web/controllers/api/account_controller.ex
@@ -4,12 +4,15 @@ defmodule GodwokenExplorerWeb.API.AccountController do
   action_fallback GodwokenExplorerWeb.API.FallbackController
 
   alias GodwokenExplorer.{Repo, Account}
+  alias GodwokenExplorer.Counters.{AddressTokenTransfersCounter, AddressTransactionsCounter}
 
   def show(conn, %{"id" => "0x" <> _} = params) do
     downcase_id = params["id"] |> String.downcase()
 
     case Account.search(downcase_id) do
-      %Account{id: id} ->
+      %Account{id: id} = account ->
+        fetch_transfer_and_transaction_count(account)
+
         result =
           id
           |> Account.find_by_id()
@@ -52,5 +55,15 @@ defmodule GodwokenExplorerWeb.API.AccountController do
       _ ->
         {:error, :not_found}
     end
+  end
+
+  defp fetch_transfer_and_transaction_count(account) do
+    Task.async(fn ->
+      AddressTokenTransfersCounter.fetch(account)
+    end)
+
+    Task.async(fn ->
+      AddressTransactionsCounter.fetch(account)
+    end)
   end
 end
