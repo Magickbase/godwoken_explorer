a771fcf7bf06ef567c5d8e15ce83cbf3f05ab3e2
Miles Zhang <mingchang555@hotmail.com>
2022-03-31 18:57:27 +0800

    feat: nil account_id join channel return error
    Signed-off-by: Miles Zhang <mingchang555@hotmail.com>

--- lib/godwoken_explorer_web/channels/account_channel.ex
+++ lib/godwoken_explorer_web/channels/account_channel.ex
@@ -9,19 +9,18 @@ defmodule GodwokenExplorerWeb.AccountChannel do
   intercept(["refresh"])
 
   def join("accounts:" <> account_id, _params, socket) do
-    result =
-      account_id
-      |> Account.find_by_id()
-      |> Account.account_to_view()
+    if account_id == "null" do
+      {:error, %{reason: "no account id"}}
+    else
+      result =
+        account_id
+        |> Account.find_by_id()
+        |> Account.account_to_view()
 
-    {:ok, result, assign(socket, :account_id, account_id)}
+      {:ok, result, assign(socket, :account_id, account_id)}
+    end
   end
 
-  @spec handle_out(
-          <<_::56>>,
-          %{:page => any, :total_count => any, :txs => any, optional(any) => any},
-          Phoenix.Socket.t()
-        ) :: {:noreply, Phoenix.Socket.t()}
   def handle_out(
         "refresh",
         account,
