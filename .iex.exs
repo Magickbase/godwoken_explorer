10ccc3162299ae17b2fbb94dfebab96b82eedfca
Naupio <naupio@163.com>
2022-04-01 11:31:50 +0800

    Fix: delete unused code and add .iex.exs

---
+++ .iex.exs
@@ -0,0 +1,25 @@
+alias GodwokenExplorer.Repo
+alias Ecto.Multi
+alias GodwokenExplorer.{
+  Account,
+  AccountUDT,
+  Block,
+  CheckInfo,
+  ContractMethod,
+  DepositHistory,
+  DailyStat,
+  Log,
+  PendingTransaction,
+  PolyjuiceCreator,
+  Polyjuice,
+  Repo,
+  SmartContract,
+  Transaction,
+  TokenTransfer,
+  UDT,
+  Version,
+  WithdrawalHistory,
+  WithdrawalRequest
+}
+
+import Ecto.{Query, Queryable, Changeset}
