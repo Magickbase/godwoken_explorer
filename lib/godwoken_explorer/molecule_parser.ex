9275cedbb69b04bfc211185fa3ec36466ee4bd46
Miles Zhang <mingchang555@hotmail.com>
2022-04-17 21:54:57 +0800

    refactor: remove unused parse witness
    Signed-off-by: Miles Zhang <mingchang555@hotmail.com>

--- lib/godwoken_explorer/molecule_parser.ex
+++ lib/godwoken_explorer/molecule_parser.ex
@@ -1,12 +1,9 @@
 defmodule Godwoken.MoleculeParser do
   use Rustler, otp_app: :godwoken_explorer, crate: :molecule_parser
 
-  # When your NIF is loaded, it will override this function.
-  def add(_a, _b), do: :erlang.nif_error(:nif_not_loaded)
   def parse_meta_contract_args(_a), do: :erlang.nif_error(:nif_not_loaded)
   def parse_global_state(_a), do: :erlang.nif_error(:nif_not_loaded)
   def parse_v0_global_state(_a), do: :erlang.nif_error(:nif_not_loaded)
-  def parse_witness(_a), do: :erlang.nif_error(:nif_not_loaded)
   def parse_deposition_lock_args(_a), do: :erlang.nif_error(:nif_not_loaded)
   def parse_withdrawal_lock_args(_a), do: :erlang.nif_error(:nif_not_loaded)
 end
