93ab883fe7a3bd00b80a27cf6cc3b46819c49a74
Miles Zhang <mingchang555@hotmail.com>
2022-04-24 12:52:12 +0800

    feat: comment parse_eth_address
    Signed-off-by: Miles Zhang <mingchang555@hotmail.com>

--- lib/godwoken_rpc/transaction.ex
+++ lib/godwoken_rpc/transaction.ex
@@ -9,19 +9,20 @@ defmodule GodwokenRPC.Transaction do
 
   @eth_addr_reg_id Application.get_env(:godwoken_explorer, :eth_addr_reg_id)
 
-  def elixir_to_params({%{
-        "block_hash" => block_hash,
-        "block_number" => block_number,
-        "raw" => %{
-          "from_id" => from_account_id,
-          "to_id" => to_account_id,
-          "nonce" => nonce,
-          "args" => "0x" <> args
-        },
-
-        "hash" => hash,
-        "eth_hash" => eth_hash
-      }, index})
+  def elixir_to_params(
+        {%{
+           "block_hash" => block_hash,
+           "block_number" => block_number,
+           "raw" => %{
+             "from_id" => from_account_id,
+             "to_id" => to_account_id,
+             "nonce" => nonce,
+             "args" => "0x" <> args
+           },
+           "hash" => hash,
+           "eth_hash" => eth_hash
+         }, index}
+      )
       when to_account_id == "0x0" do
     {{code_hash, hash_type, script_args}, fee_amount_hex_string} = parse_meta_contract_args(args)
 
@@ -47,18 +48,20 @@ defmodule GodwokenRPC.Transaction do
     }
   end
 
-  def elixir_to_params({%{
-        "block_hash" => block_hash,
-        "block_number" => block_number,
-        "raw" => %{
-          "from_id" => from_account_id,
-          "to_id" => to_id,
-          "nonce" => nonce,
-          "args" => "0x" <> args
-        },
-        "hash" => hash,
-        "eth_hash" => eth_hash
-      }, index}) do
+  def elixir_to_params(
+        {%{
+           "block_hash" => block_hash,
+           "block_number" => block_number,
+           "raw" => %{
+             "from_id" => from_account_id,
+             "to_id" => to_id,
+             "nonce" => nonce,
+             "args" => "0x" <> args
+           },
+           "hash" => hash,
+           "eth_hash" => eth_hash
+         }, index}
+      ) do
     from_account_id = hex_to_number(from_account_id)
     to_account_id = hex_to_number(to_id)
 
@@ -87,21 +90,21 @@ defmodule GodwokenRPC.Transaction do
         }
 
       to_id == @eth_addr_reg_id ->
-        case parse_eth_address_registry_args(args) do
-          {"EthToGw", eth_address, _} ->
-            Logger.info("===========ETHToGw#{eth_address}")
+        # case parse_eth_address_registry_args(args) do
+        #   {"EthToGw", eth_address, _} ->
+        #     Logger.info("===========ETHToGw#{eth_address}")
 
-          {"GwToEth", gw_script_hash, _} ->
-            Logger.info("===========GwToEth#{gw_script_hash}")
+        #   {"GwToEth", gw_script_hash, _} ->
+        #     Logger.info("===========GwToEth#{gw_script_hash}")
 
-          {"SetMapping", gw_script_hash, fee} ->
-            Logger.info("===========SetMapping#{gw_script_hash}#{fee}")
+        #   {"SetMapping", gw_script_hash, fee} ->
+        #     Logger.info("===========SetMapping#{gw_script_hash}#{fee}")
 
-          {"BatchSetMapping", gw_script_hashes, fee} ->
-            Logger.info("===========BatchSetMapping#{gw_script_hashes}#{fee}")
-            {hashes_count, str_hashes} = gw_script_hashes |> String.split_at(8)
-            hashes = for <<x::binary-64 <- str_hashes>>, do: x
-        end
+        #   {"BatchSetMapping", gw_script_hashes, fee} ->
+        #     Logger.info("===========BatchSetMapping#{gw_script_hashes}#{fee}")
+        #     {hashes_count, str_hashes} = gw_script_hashes |> String.split_at(8)
+        #     hashes = for <<x::binary-64 <- str_hashes>>, do: x
+        # end
 
         %{
           type: :eth_address_registry,
