ccd3f09fd66c580da631194af757c7f4fd6517e7
Miles Zhang <mingchang555@hotmail.com>
2022-04-26 17:08:13 +0800

    feat: rename short_address to eth_address
    Signed-off-by: Miles Zhang <mingchang555@hotmail.com>

--- lib/godwoken_explorer_web/views/udt_view.ex
+++ lib/godwoken_explorer_web/views/udt_view.ex
@@ -16,7 +16,7 @@ defmodule GodwokenExplorer.UDTView do
       :supply,
       :holder_count,
       :type,
-      :short_address,
+      :eth_address,
       :type_script,
       :official_site,
       :description,
@@ -26,7 +26,7 @@ defmodule GodwokenExplorer.UDTView do
     ]
   end
 
-  def short_address(udt, _conn) do
+  def eth_address(udt, _conn) do
     udt.account.eth_address
   end
 
