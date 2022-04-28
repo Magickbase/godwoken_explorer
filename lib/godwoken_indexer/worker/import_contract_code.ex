acf6263a0faac80e7923001cf773649f88a6287d
Miles Zhang <mingchang555@hotmail.com>
2022-04-28 20:55:20 +0800

    feat: generate contract that byte code same as yok
    Signed-off-by: Miles Zhang <mingchang555@hotmail.com>

--- lib/godwoken_indexer/worker/import_contract_code.ex
+++ lib/godwoken_indexer/worker/import_contract_code.ex
@@ -13,16 +13,27 @@ defmodule GodwokenIndexer.Worker.ImportContractCode do
        ]
      }} = GodwokenRPC.fetch_codes([%{block_quantity: block_number, address: address}])
 
-    case Account.search(address) do
-      %Account{} = account ->
-        account |> Account.changeset(%{contract_code: contract_code}) |> Repo.update()
+    account =
+      case Account.search(address) do
+        %Account{} = account ->
+          account |> Account.changeset(%{contract_code: contract_code}) |> Repo.update()
 
-      nil ->
-        {:ok, account_id} = Account.find_by_short_address(address)
-        {:ok, account} = Account.manual_create_account!(account_id)
-        account |> Account.changeset(%{contract_code: contract_code}) |> Repo.update()
-    end
+        nil ->
+          {:ok, account_id} = Account.find_by_short_address(address)
+          {:ok, account} = Account.manual_create_account!(account_id)
+          account |> Account.changeset(%{contract_code: contract_code}) |> Repo.update()
+      end
 
+    compare_with_yok_contract(contract_code, account.id)
     :ok
+  end
+
+  defp compare_with_yok_contract(contract_code, account_id) do
+    if System.get_env("GODWOKEN_CHAIN") == "mainnet" &&
+         Account.yok_contract_code() == contract_code do
+      %{account_id: account_id}
+      |> GodwokenIndexer.Worker.CopyYokContract.new()
+      |> Oban.insert()
+    end
   end
 end
