61e0e085d8148f2e9b32d11634c18dae1b96cd22
Miles Zhang <mingchang555@hotmail.com>
2022-05-18 15:32:55 +0800

    feat: event api for contract address hash
    Signed-off-by: Miles Zhang <mingchang555@hotmail.com>

--- lib/godwoken_explorer_web/views/log_view.ex
+++ lib/godwoken_explorer_web/views/log_view.ex
@@ -9,6 +9,8 @@ defmodule GodwokenExplorer.LogView do
     [
       :data,
       :address_hash,
+      :block_number,
+      :transaction_hash,
       :first_topic,
       :second_topic,
       :third_topic,
@@ -31,6 +33,33 @@ defmodule GodwokenExplorer.LogView do
         data: l.data,
         index: l.index,
         address_hash: l.address_hash,
+        first_topic: l.first_topic,
+        second_topic: l.second_topic,
+        third_topic: l.third_topic,
+        fourth_topic: l.fourth_topic,
+        abi: s.abi
+      },
+      order_by: [desc: :block_number, desc: :index]
+    )
+    |> Repo.paginate(page: page, page_size: page_size)
+  end
+
+  def list_by_address_hash(address_hash, page, page_size) do
+    from(
+      l in Log,
+      left_join: a in Account,
+      on: a.short_address == l.address_hash,
+      left_join: s in SmartContract,
+      on: s.account_id == a.id,
+      join: t in Transaction,
+      on: t.hash == l.transaction_hash,
+      where: l.address_hash == ^address_hash,
+      select: %{
+        data: l.data,
+        index: l.index,
+        transaction_hash: l.transaction_hash,
+        address_hash: l.address_hash,
+        block_number: t.block_number,
         first_topic: l.first_topic,
         second_topic: l.second_topic,
         third_topic: l.third_topic,
