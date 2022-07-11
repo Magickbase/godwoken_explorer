1f9eeffa8d7231b3b8c60380d68a091b1b09f464
Miles Zhang <mingchang555@hotmail.com>
2022-07-11 10:11:56 +0800

    feat: add eth_type to udts
    Signed-off-by: Miles Zhang <mingchang555@hotmail.com>

---
+++ priv/repo/migrations/20220711015030_add_eth_type_to_udt.exs
@@ -0,0 +1,12 @@
+defmodule GodwokenExplorer.Repo.Migrations.AddEthTypeToUDT do
+  use Ecto.Migration
+
+  def change do
+    alter table(:udts) do
+      add :eth_type, :string
+      add :contract_address_hash, :bytea
+    end
+
+    create unique_index(:udts, [:contract_address_hash])
+  end
+end
