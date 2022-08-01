3cd67b1c5c7076dd81ce24e857ca4095bcf4327c
Miles Zhang <mingchang555@hotmail.com>
2022-08-01 14:56:45 +0800

    fix: if no records return []
    Signed-off-by: Miles Zhang <mingchang555@hotmail.com>

--- test/godwoken_explorer_web/controllers/api/transfer_controller_test.exs
+++ test/godwoken_explorer_web/controllers/api/transfer_controller_test.exs
@@ -1,7 +1,14 @@
 defmodule GodwokenExplorerWeb.API.TransferControllerTest do
   use GodwokenExplorerWeb.ConnCase
 
-  describe "index" do
+  import GodwokenExplorer.Factory
+
+  import GodwokenRPC.Util,
+    only: [
+      balance_to_view: 2
+    ]
+
+  describe "json" do
     test "when eth address account not exist", %{conn: conn} do
       conn =
         get(
@@ -25,6 +32,50 @@ defmodule GodwokenExplorerWeb.API.TransferControllerTest do
 
       assert json_response(conn, 200) ==
                %{"page" => 1, "total_count" => 0, "txs" => []}
+    end
+  end
+
+  describe "export" do
+    setup do
+      tx = insert(:transaction) |> with_polyjuice()
+      %{tx: tx}
+    end
+
+    test "when tx have no erc20 records", %{conn: conn, tx: tx} do
+      conn =
+        get(
+          conn,
+          Routes.transfer_path(conn, :index,
+            tx_hash: tx.eth_hash |> to_string(),
+            export: true
+          )
+        )
+
+      assert conn.resp_body |> String.split("\r\n") |> Enum.drop(1) == [""]
+    end
+
+    test "when tx have erc20 records", %{conn: conn, tx: tx} do
+      udt = insert(:native_udt)
+
+      tt =
+        insert(:token_transfer,
+          transaction: tx,
+          token_contract_address_hash: udt.contract_address_hash
+        )
+
+      conn =
+        get(
+          conn,
+          Routes.transfer_path(conn, :index,
+            tx_hash: tx.eth_hash |> to_string(),
+            export: true
+          )
+        )
+
+      assert conn.resp_body |> String.split("\r\n") |> Enum.drop(1) == [
+               "#{tt.from_address_hash |> to_string},#{tt.to_address_hash |> to_string()},#{tt.amount |> balance_to_view(udt.decimal)}",
+               ""
+             ]
     end
   end
 end
