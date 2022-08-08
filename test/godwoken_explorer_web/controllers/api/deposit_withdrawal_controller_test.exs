7dfde6d8c3ba612a9a575a1d43e489a51e1f1179
Miles Zhang <mingchang555@hotmail.com>
2022-08-08 16:21:59 +0800

    feat: query native udt but return brit's bridge udt's deposit and withdrawal records
    Signed-off-by: Miles Zhang <mingchang555@hotmail.com>

--- test/godwoken_explorer_web/controllers/api/deposit_withdrawal_controller_test.exs
+++ test/godwoken_explorer_web/controllers/api/deposit_withdrawal_controller_test.exs
@@ -5,7 +5,8 @@ defmodule GodwokenExplorerWeb.API.DepositWithdrawalControllerTest do
   import GodwokenRPC.Util, only: [balance_to_view: 2]
 
   setup do
-    udt = insert(:ckb_udt)
+    native_udt = insert(:native_udt)
+    udt = insert(:ckb_udt, bridge_account_id: native_udt.id)
     insert(:ckb_account)
     user = insert(:user)
     block = insert(:block)
@@ -19,7 +20,14 @@ defmodule GodwokenExplorerWeb.API.DepositWithdrawalControllerTest do
         block_hash: block.hash
       )
 
-    %{deposit: deposit, user: user, udt: udt, withdrawal: withdrawal, block: block}
+    %{
+      deposit: deposit,
+      user: user,
+      udt: udt,
+      withdrawal: withdrawal,
+      block: block,
+      native_udt: native_udt
+    }
   end
 
   describe "index" do
@@ -81,7 +89,7 @@ defmodule GodwokenExplorerWeb.API.DepositWithdrawalControllerTest do
                }
     end
 
-    test "lists by udt_id", %{
+    test "udt is bridge", %{
       conn: conn,
       deposit: deposit,
       user: user,
@@ -93,6 +101,74 @@ defmodule GodwokenExplorerWeb.API.DepositWithdrawalControllerTest do
         get(
           conn,
           Routes.deposit_withdrawal_path(conn, :index, udt_id: udt.id)
+        )
+
+      assert json_response(conn, 200) ==
+               %{
+                 "data" => [
+                   %{
+                     "block_hash" => to_string(block.hash),
+                     "block_number" => block.number,
+                     "ckb_lock_hash" => nil,
+                     "eth_address" => to_string(user.eth_address),
+                     "layer1_block_number" => withdrawal.layer1_block_number,
+                     "layer1_output_index" => withdrawal.layer1_output_index,
+                     "layer1_tx_hash" => to_string(withdrawal.layer1_tx_hash),
+                     "owner_lock_hash" => to_string(withdrawal.owner_lock_hash),
+                     "script_hash" => to_string(withdrawal.l2_script_hash),
+                     "sudt_script_hash" => to_string(udt.script_hash),
+                     "timestamp" => withdrawal.timestamp |> DateTime.to_iso8601(),
+                     "type" => "withdrawal",
+                     "udt_icon" => udt.icon,
+                     "udt_id" => udt.id,
+                     "udt_name" => udt.name,
+                     "udt_symbol" => udt.symbol,
+                     "value" => withdrawal.amount |> balance_to_view(udt.decimal),
+                     "state" => "pending",
+                     "capacity" => withdrawal.capacity |> Decimal.to_string(),
+                     "udt_decimal" => udt.decimal
+                   },
+                   %{
+                     "block_hash" => nil,
+                     "block_number" => nil,
+                     "ckb_lock_hash" => to_string(deposit.ckb_lock_hash),
+                     "eth_address" => to_string(user.eth_address),
+                     "layer1_block_number" => deposit.layer1_block_number,
+                     "layer1_output_index" => deposit.layer1_output_index,
+                     "layer1_tx_hash" => to_string(deposit.layer1_tx_hash),
+                     "owner_lock_hash" => nil,
+                     "script_hash" => to_string(deposit.script_hash),
+                     "sudt_script_hash" => nil,
+                     "timestamp" => deposit.timestamp |> DateTime.to_iso8601(),
+                     "type" => "deposit",
+                     "udt_icon" => udt.icon,
+                     "udt_id" => udt.id,
+                     "udt_name" => udt.name,
+                     "udt_symbol" => udt.symbol,
+                     "value" => deposit.amount |> balance_to_view(udt.decimal),
+                     "state" => "succeed",
+                     "capacity" => deposit.capacity |> Decimal.to_string(),
+                     "udt_decimal" => udt.decimal
+                   }
+                 ],
+                 "page" => "1",
+                 "total_count" => "2"
+               }
+    end
+
+    test "udt is native", %{
+      conn: conn,
+      deposit: deposit,
+      user: user,
+      udt: udt,
+      withdrawal: withdrawal,
+      block: block,
+      native_udt: native_udt
+    } do
+      conn =
+        get(
+          conn,
+          Routes.deposit_withdrawal_path(conn, :index, udt_id: native_udt.id)
         )
 
       assert json_response(conn, 200) ==
