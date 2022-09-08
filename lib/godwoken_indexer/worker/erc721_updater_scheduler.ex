5ea568c4a70b1034db07e36ec80d474ddac0c854
Naupio <naupio@163.com>
2022-09-08 12:30:43 +0800

    Fix: fix ERC721UpdaterScheduler

--- lib/godwoken_indexer/worker/erc721_updater_scheduler.ex
+++ lib/godwoken_indexer/worker/erc721_updater_scheduler.ex
@@ -2,10 +2,10 @@ defmodule GodwokenIndexer.Worker.ERC721UpdaterScheduler do
   use Oban.Worker, queue: :default
 
   import Ecto.Query, only: [from: 2]
-
+  import GodwokenRPC.Util, only: [import_timestamps: 0]
+  alias GodwokenExplorer.Chain.{Import}
   alias GodwokenExplorer.{Repo, UDT}
-
-  alias GodwokenIndexer.Worker.ERC721Updater, as: ERC721UpdaterWorker
+  alias GodwokenExplorer.Token.MetadataRetriever
 
   @impl Oban.Worker
   def perform(%Oban.Job{}) do
@@ -14,37 +14,65 @@ defmodule GodwokenIndexer.Worker.ERC721UpdaterScheduler do
   end
 
   def do_perform() do
+    get_unfetched_udts()
+    |> Enum.chunk_every(10)
+    |> Enum.map(fn chunk_unfetched_udts ->
+      need_update_list =
+        Task.async_stream(chunk_unfetched_udts, fn chunk_unfetched_udt ->
+          contract_address_hash = chunk_unfetched_udt.contract_address_hash
+          return = MetadataRetriever.get_functions_of(contract_address_hash)
+
+          need_update =
+            chunk_unfetched_udt
+            |> Map.merge(return)
+            |> Map.take([
+              :id,
+              :name,
+              :totalSupply,
+              :decimals,
+              :symbol,
+              :update_at,
+              :contract_address_hash
+            ])
+
+          need_update
+        end)
+        |> Enum.map(fn {:ok, r} -> r end)
+
+      Import.insert_changes_list(
+        need_update_list |> Enum.map(fn udt -> Map.delete(udt, :contract_address_hash) end),
+        for: UDT,
+        timestamps: import_timestamps(),
+        on_conflict: {:replace, [:name, :symbol, :updated_at]},
+        conflict_target: :id
+      )
+    end)
+  end
+
+  def get_unfetched_udts() do
     datetime = Timex.now() |> Timex.shift(hours: -1)
 
-    unfetched_udts =
-      from(u in UDT,
-        where:
-          u.type == :native and u.eth_type == :erc721 and
-            (is_nil(u.name) or is_nil(u.symbol)) and
-            u.updated_at < ^datetime,
-        order_by: [desc: u.id]
-      )
-      |> Repo.all()
-
-    Enum.chunk_every(unfetched_udts, 50)
-    |> Enum.map(fn chunk_unfetched_udts ->
-      chunk_unfetched_udts
-      |> Enum.map(fn u ->
-        u
-        |> Map.from_struct()
-        |> Map.take([
-          :id,
-          :name,
-          :totalSupply,
-          :decimals,
-          :symbol,
-          :update_at,
-          :contract_address_hash
-        ])
-      end)
-    end)
-    |> Enum.each(fn chunk_unfetched_udts ->
-      ERC721UpdaterWorker.new(%{chunk_unfetched_udts: chunk_unfetched_udts}) |> Oban.insert()
+    from(u in UDT,
+      where:
+        u.type == :native and u.eth_type == :erc721 and
+          (is_nil(u.name) or is_nil(u.symbol)) and
+          u.updated_at < ^datetime,
+      order_by: [desc: u.id],
+      limit: 100
+    )
+    |> Repo.all()
+    |> Enum.map(fn chunk_unfetched_udt ->
+      chunk_unfetched_udt
+      |> Map.from_struct()
+      |> Map.take([
+        :id,
+        :name,
+        :totalSupply,
+        :decimals,
+        :symbol,
+        :update_at,
+        :contract_address_hash
+      ])
     end)
   end
 end
