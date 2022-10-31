f51ba324980e8678576a30ab535b6e5fb2ac76ae
Naupio <naupio@163.com>
2022-10-31 08:54:46 +0800

    Docs: add token_instance object docs

--- lib/godwoken_explorer/graphql/types/token_instance.ex
+++ lib/godwoken_explorer/graphql/types/token_instance.ex
@@ -2,9 +2,9 @@ defmodule GodwokenExplorer.Graphql.Types.TokenInstance do
   use Absinthe.Schema.Notation
 
   object :token_instance do
-    field(:token_contract_address_hash, :hash_address)
-    field(:token_id, :decimal)
-    field(:metadata, :json)
-    field(:error, :string)
+    field :token_contract_address_hash, :hash_address, description: "Address hash foreign key."
+    field :token_id, :decimal, description: "ID of the token."
+    field :metadata, :json, description: "Token instance metadata."
+    field :error, :string, description: "Error of fetching token instance."
   end
 end
