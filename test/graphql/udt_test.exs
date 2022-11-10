cf66e340612b0a7dcf1dbbeb38193cb33e6fdf53
Miles Zhang <mingchang555@hotmail.com>
2022-11-10 19:17:22 +0800

    test: add more test
    Signed-off-by: Miles Zhang <mingchang555@hotmail.com>

--- test/graphql/udt_test.exs
+++ test/graphql/udt_test.exs
@@ -3,6 +3,8 @@ defmodule GodwokenExplorer.Graphql.UDTTest do
 
   import GodwokenExplorer.Factory, only: [insert!: 1, insert!: 2, address_hash: 0, insert: 2]
 
+  alias GodwokenExplorer.{Repo, UDT}
+
   setup do
     {:ok, script_hash} =
       GodwokenExplorer.Chain.Hash.cast(
@@ -123,7 +125,7 @@ defmodule GodwokenExplorer.Graphql.UDTTest do
     ]
   end
 
-  test "bridge udt name symbol", %{conn: conn, ckb_udt: ckb_udt} do
+  test "show bridge udt's uan as name symbol", %{conn: conn, ckb_udt: ckb_udt} do
     query = """
     query {
       udt(
@@ -147,6 +149,37 @@ defmodule GodwokenExplorer.Graphql.UDTTest do
                  "udt" => %{
                    "name" => ckb_udt.display_name,
                    "symbol" => ckb_udt.uan
+                 }
+               }
+             }
+  end
+
+  test "show bridge udt's name symbol", %{conn: conn, ckb_udt: ckb_udt} do
+    query = """
+    query {
+      udt(
+        input: { id: #{ckb_udt.id} }
+      ) {
+        name
+        symbol
+      }
+    }
+    """
+
+    UDT.changeset(ckb_udt, %{display_name: nil, uan: nil}) |> Repo.update()
+
+    conn =
+      post(conn, "/graphql", %{
+        "query" => query,
+        "variables" => %{}
+      })
+
+    assert json_response(conn, 200) ==
+             %{
+               "data" => %{
+                 "udt" => %{
+                   "name" => ckb_udt.name,
+                   "symbol" => ckb_udt.symbol
                  }
                }
              }
