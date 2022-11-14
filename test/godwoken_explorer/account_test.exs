ccb593f83d6cc744da3e89b8ba0f61e4ff8de990
Miles Zhang <mingchang555@hotmail.com>
2022-11-14 10:43:55 +0800

    test: add test
    Signed-off-by: Miles Zhang <mingchang555@hotmail.com>

--- test/godwoken_explorer/account_test.exs
+++ test/godwoken_explorer/account_test.exs
@@ -2,6 +2,7 @@ defmodule GodwokenExplorer.AccountTest do
   use GodwokenExplorer.DataCase
 
   import Mock
+  import Hammox
 
   alias GodwokenExplorer.{Account, Repo}
 
@@ -58,6 +59,51 @@ defmodule GodwokenExplorer.AccountTest do
 
       assert account.script_hash |> to_string() ==
                "0x74db559cbb4928e1c2dfb6ab30623bb338a53904f47458c02a07be01c84b1af9"
+    end
+  end
+
+  describe "find_or_create_contract_by_eth_address/1" do
+    test "when account not exist in chain" do
+      MockGodwokenRPC
+      |> expect(:fetch_account_id, fn _script_hash ->
+        {:error, :account_slow}
+      end)
+
+      assert Account.find_or_create_contract_by_eth_address(
+               "0x17f01ec05a4a6a43857e3bb8d8ddd68e8d1d185f"
+             ) == {:error, nil}
+    end
+
+    test "when account exist in chain" do
+      MockGodwokenRPC
+      |> expect(:fetch_account_id, fn _script_hash ->
+        {:ok, 48250}
+      end)
+      |> expect(:fetch_nonce, fn id ->
+        assert id == 48250
+
+        10
+      end)
+      |> expect(:fetch_script, fn script_hash ->
+        assert script_hash == "0x74db559cbb4928e1c2dfb6ab30623bb338a53904f47458c02a07be01c84b1af9"
+
+        {:ok,
+         %{
+           "args" =>
+             "0x702359ea7f073558921eb50d8c1c77e92f760c8f8656bde4995f26b8963e2dd80400000017f01ec05a4a6a43857e3bb8d8ddd68e8d1d185f",
+           "code_hash" => "0x1629b04b49ded9e5747481f985b11cba6cdd4ffc167971a585e96729455ca736",
+           "hash_type" => "type"
+         }}
+      end)
+      |> expect(:fetch_script_hash, fn params ->
+        assert params == %{account_id: 48250}
+
+        {:ok, "0x74db559cbb4928e1c2dfb6ab30623bb338a53904f47458c02a07be01c84b1af9"}
+      end)
+
+      assert Account.find_or_create_contract_by_eth_address(
+               "0x17f01ec05a4a6a43857e3bb8d8ddd68e8d1d185f"
+             ) == {:ok, Repo.get(Account, 48250)}
     end
   end
 end
