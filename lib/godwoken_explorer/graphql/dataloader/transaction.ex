85589e23de25a7782cb5e1903685f34cf5b1b2cd
Naupio <naupio@163.com>
2022-11-30 00:00:02 +0800

    Feat: add dataloader for transaction object

--- lib/godwoken_explorer/graphql/dataloader/transaction.ex
+++
@@ -1,9 +0,0 @@
-defmodule GodwokenExplorer.Graphql.Dataloader.Transaction do
-  def data() do
-    Dataloader.Ecto.new(GodwokenExplorer.Repo, query: &query/2)
-  end
-
-  def query(queryable, _params) do
-    queryable
-  end
-end
