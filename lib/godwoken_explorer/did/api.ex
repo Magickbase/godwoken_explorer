adc8c359af6ca1b8521057257089154a9d652a97
Miles Zhang <mingchang555@hotmail.com>
2022-12-12 18:41:22 +0800

    feat: fetch .bit reverse record info
    Signed-off-by: Miles Zhang <mingchang555@hotmail.com>

---
+++ lib/godwoken_explorer/did/api.ex
@@ -0,0 +1,44 @@
+defmodule GodwokenExplorer.Did.API do
+  require Logger
+
+  def fetch_reverse_record_info(address) do
+    url = "#{base_url()}/v1/reverse/record"
+
+    request = %{
+      type: "blockchain",
+      key_info: %{
+        # 60: ETH, 195: TRX, 9006: BNB, 966: Matic
+        coin_type: "60",
+        # 1: ETH, 56: BSC, 137: Polygon
+        chain_id: "1",
+        key: "#{address}"
+      }
+    }
+
+    case HTTPoison.post(url, Jason.encode_to_iodata!(request), [
+           {"Content-Type", "application/json"}
+         ]) do
+      {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
+        case Jason.decode!(body) do
+          %{
+            "data" => %{"account" => _, "account_alias" => account_alias},
+            "errno" => 0,
+            "errmsg" => ""
+          } ->
+            {:ok, account_alias}
+
+          %{"data" => _data, "errno" => errno, "errmsg" => errmsg} ->
+            Logger.error("Fetch #{address} account alias failed.#{errno}: #{errmsg}")
+            {:error, nil}
+        end
+
+      {:error, %HTTPoison.Error{reason: reason}} ->
+        Logger.error("Fetch #{address} account alias failed.reason: #{reason}")
+        {:error, nil}
+    end
+  end
+
+  defp base_url do
+    Application.fetch_env!(:godwoken_explorer, :did)[:indexer_url]
+  end
+end
