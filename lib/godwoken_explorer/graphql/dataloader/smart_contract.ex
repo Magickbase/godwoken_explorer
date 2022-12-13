5ba655c055de8ed349b28b9066dfaf6d6b43b3c0
Naupio <naupio@163.com>
2022-12-14 01:22:20 +0800

    feat: optimize smart contract object with batch and dataloader

--- lib/godwoken_explorer/graphql/dataloader/smart_contract.ex
+++ lib/godwoken_explorer/graphql/dataloader/smart_contract.ex
@@ -1,8 +1,9 @@
 defmodule GodwokenExplorer.Graphql.Dataloader.BatchSmartContract do
   import Ecto.Query
 
-  alias GodwokenExplorer.Account
   alias GodwokenExplorer.Repo
+  alias GodwokenExplorer.Account
+  alias GodwokenExplorer.Account.CurrentUDTBalance
 
   def account(Account, ids) do
     from(a in Account,
@@ -11,6 +12,27 @@ defmodule GodwokenExplorer.Graphql.Dataloader.BatchSmartContract do
     |> Repo.all()
     |> Map.new(fn a ->
       {a.id, a}
+    end)
+  end
+
+  def ckb_balance(CurrentUDTBalance, ids) do
+    accounts =
+      from(a in Account,
+        where: a.id in ^ids
+      )
+      |> Repo.all()
+
+    eth_addresses =
+      accounts
+      |> Enum.map(& &1.eth_address)
+
+    accounts_id_address_map =
+      accounts
+      |> Enum.into(%{}, fn a -> {a.eth_address, a.id} end)
+
+    CurrentUDTBalance.get_ckb_balance(eth_addresses)
+    |> Enum.into(%{}, fn %{address: address, balance: balance} ->
+      {Map.get(accounts_id_address_map, address), balance}
     end)
   end
 end
