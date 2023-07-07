d2108bb57de207f3782dff9fb48e39e11bd6877b
Miles Zhang <mingchang555@hotmail.com>
2023-07-08 05:44:18 +0800

    fix: rename method get_tip to get_indexer_tip
    Signed-off-by: Miles Zhang <mingchang555@hotmail.com>

--- lib/godwoken_rpc/ckb_indexer/fetched_tip.ex
+++ lib/godwoken_rpc/ckb_indexer/fetched_tip.ex
@@ -1,5 +1,5 @@
 defmodule GodwokenRPC.CKBIndexer.FetchedTip do
   def request do
-    GodwokenRPC.request(%{id: 2, method: "get_tip"})
+    GodwokenRPC.request(%{id: 2, method: "get_indexer_tip"})
   end
 end
